182d380b97e0fe19bbe9cbba9f8d11e578d7bfa1
Syarif <syarif@sukamart.com>
2019-06-18 18:14:06 +0700

    Go Gin Template Html

--- example.go
+++ example.go
@@ -1,12 +1,25 @@
 package main
-import "github.com/gin-gonic/gin"
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
 
 func main(){
-  r := gin.Default()
-  r.GET("/ping",func(c *gin.Context){
-    c.JSON(200, gin.H{
-      "message":"pong",
+    r := gin.Default()
+    r.LoadHTMLGlob("templates/*");
+    r.GET("/ping",func(c *gin.Context){
+        c.JSON(200, gin.H{
+            "message":"pong",
+        });
     })
-  })
-  r.Run()
+
+    r.GET("/array", func(c *gin.Context){
+        var values []int
+        for i := 0; i < 5; i++ {
+            values = append(values, i)
+        }
+        c.HTML(http.StatusOK, "array.tmpl", gin.H{"values": values})
+    });
+    
+    r.Run()
 }
